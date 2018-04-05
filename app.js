243e58497d93feebd4ea8ccf2a8aef777ad72e82
jlehm <jlehm@protonmail.com>
2018-04-05 10:33:01 -0500

    added fonts scss, edited webpack.config.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import './app/css/style.css';
 import './app/scss/style.scss';
+import './app/scss/fonts.scss';
 
 console.log('loading complete');
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,10 +3,11 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 var path = require("path");
 
 module.exports = {
+
     entry: "./app.js", //relative to root of the application
     output: {
-        path: path.join(__dirname, "./dist/"),
-        filename: "./dist/app.bundle.js" //relative to root of the application
+      path: path.join(__dirname, "./dist/"),
+      filename: "./dist/app.bundle.js" //relative to root of the application
     },
     watch:true,
     devServer: {
@@ -23,24 +24,39 @@ module.exports = {
           use: [{
               loader: 'url-loader',
               options: {
-                  limit: 8000, // images < 8kb converted to base64 strings
+                  limit: 8000, // Convert images < 8kb to base64 strings
                   name: 'images/[hash]-[name].[ext]'
+              }
+          }]
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [{
+              loader: 'file-loader',
+              options: {
+                  name: '[name].[ext]',
+                  outputPath: 'fonts/'
               }
           }]
         }
       ]
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            hash: true,
-            title: 'static-webpack',
-            template: './app/html/index.html',
-            path: path.join(__dirname, "../dist/"),
-            //filename: './dist/index.html' //relative to root of the application
-        }),
-        new CopyWebpackPlugin([{
-            from:'app/images',
-            to:'images'
-        }])
-   ]
+      new HtmlWebpackPlugin({
+          hash: true,
+          title: 'static-webpack',
+          template: './app/html/index.html',
+          path: path.join(__dirname, "../dist/"),
+          //filename: './dist/index.html' //relative to root of the application
+      }),
+      new CopyWebpackPlugin([{
+          from:'app/images',
+          to:'images'
+      }]),
+/*      new CopyWebpackPlugin([{
+          from:'app/fonts',
+          to:'fonts'
+      }])
+*/ ]
+
 }
